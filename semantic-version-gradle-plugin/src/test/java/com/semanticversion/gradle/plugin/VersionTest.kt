6df81ddf0b5e959f0d70488f172277627ee48309
Maxi Rosson <maxirosson@gmail.com>
2020-05-23 09:42:55 -0300

    Added more test cases

--- semantic-version-gradle-plugin/src/test/java/com/semanticversion/gradle/plugin/VersionTest.kt
+++ semantic-version-gradle-plugin/src/test/java/com/semanticversion/gradle/plugin/VersionTest.kt
@@ -8,53 +8,95 @@ import org.junit.Test
 class VersionTest {
 
     @Test
-    fun validVersion() {
+    fun `GIVEN a valid snapshot version WHEN creating a version THEN it is successfully created`() {
         var version = createVersion("1.2.3")
 
         Truth.assertThat(version.versionMajor).isEqualTo(1)
         Truth.assertThat(version.versionMinor).isEqualTo(2)
         Truth.assertThat(version.versionPatch).isEqualTo(3)
+        Truth.assertThat(version.isSnapshot).isTrue()
+        Truth.assertThat(version.isLocal).isFalse()
+        Truth.assertThat(version.isVersionTimestampEnabled).isFalse()
+        Truth.assertThat(version.featureName).isNull()
+        Truth.assertThat(version.versionClassifier).isEqualTo("SNAPSHOT")
+        Truth.assertThat(version.baseVersion).isEqualTo("1.2.3")
         Truth.assertThat(version.toString()).isEqualTo("1.2.3-SNAPSHOT")
 
         version = createVersion("111.222.333")
         Truth.assertThat(version.versionMajor).isEqualTo(111)
         Truth.assertThat(version.versionMinor).isEqualTo(222)
         Truth.assertThat(version.versionPatch).isEqualTo(333)
+        Truth.assertThat(version.isSnapshot).isTrue()
+        Truth.assertThat(version.isLocal).isFalse()
+        Truth.assertThat(version.isVersionTimestampEnabled).isFalse()
+        Truth.assertThat(version.featureName).isNull()
+        Truth.assertThat(version.versionClassifier).isEqualTo("SNAPSHOT")
+        Truth.assertThat(version.baseVersion).isEqualTo("111.222.333")
         Truth.assertThat(version.toString()).isEqualTo("111.222.333-SNAPSHOT")
     }
 
     @Test(expected = RuntimeException::class)
-    fun notSemanticVersion() {
+    fun `GIVEN a version with 4 segments WHEN creating a version THEN it throws an exception`() {
         createVersion("1.2.3.4")
     }
 
     @Test(expected = RuntimeException::class)
-    fun notSemanticVersion2() {
+    fun `GIVEN a version with 4 segments WHEN creating a version using the version constructor THEN it throws an exception`() {
+        Version("1.2.3.4")
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun `GIVEN a version with 2 segments WHEN creating a version THEN it throws an exception`() {
         createVersion("1.2")
     }
 
     @Test(expected = RuntimeException::class)
-    fun notSemanticVersion3() {
+    fun `GIVEN a version with 2 segments WHEN creating a version using the version constructor THEN it throws an exception`() {
+        Version("1.2")
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun `GIVEN a version with 1 segments WHEN creating a version THEN it throws an exception`() {
         createVersion("1")
     }
 
     @Test(expected = RuntimeException::class)
-    fun invalidMajor() {
+    fun `GIVEN a version with 1 segments WHEN creating a version using the version constructor THEN it throws an exception`() {
+        Version("1")
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun `GIVEN a version with invalid major WHEN creating a version THEN it throws an exception`() {
         createVersion("1111.2.3")
     }
 
     @Test(expected = RuntimeException::class)
-    fun invalidMinor() {
+    fun `GIVEN a version with invalid major WHEN creating a version using the version constructor THEN it throws an exception`() {
+        Version("1111.2.3")
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun `GIVEN a version with invalid minor WHEN creating a version THEN it throws an exception`() {
         createVersion("1.2222.3")
     }
 
     @Test(expected = RuntimeException::class)
-    fun invalidPatch() {
+    fun `GIVEN a version with invalid minor WHEN creating a version using the version constructor THEN it throws an exception`() {
+        Version("1.2222.3")
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun `GIVEN a version with invalid patch WHEN creating a version THEN it throws an exception`() {
         createVersion("1.2.3333")
     }
 
+    @Test(expected = RuntimeException::class)
+    fun `GIVEN a version with invalid patch WHEN creating a version using the version constructor THEN it throws an exception`() {
+        Version("1.2.3333")
+    }
+
     @Test
-    fun incrementMajor() {
+    fun `GIVEN a version WHEN creating invoking incrementMajor() THEN it increments major and reset minor and patch`() {
         val version = createVersion("1.2.3")
         version.incrementMajor()
         Truth.assertThat(version.versionMajor).isEqualTo(2)
@@ -63,20 +105,23 @@ class VersionTest {
     }
 
     @Test(expected = RuntimeException::class)
-    fun invalidIncrementMajor() {
+    fun `GIVEN a version with the maximum possible major WHEN creating invoking incrementMajor() THEN it throws an exception`() {
         val version = createVersion("999.2.3")
         version.incrementMajor()
     }
 
     @Test
-    fun incrementMinor() {
-        var version = createVersion("1.2.3")
+    fun `GIVEN a version WHEN creating invoking incrementMinor() THEN it increments minor and reset the patch`() {
+        val version = createVersion("1.2.3")
         version.incrementMinor()
         Truth.assertThat(version.versionMajor).isEqualTo(1)
         Truth.assertThat(version.versionMinor).isEqualTo(3)
         Truth.assertThat(version.versionPatch).isEqualTo(0)
+    }
 
-        version = createVersion("1.999.3")
+    @Test
+    fun `GIVEN a version with the maximum minor WHEN creating invoking incrementMinor() THEN it increments major and reset minor & patch`() {
+        val version = createVersion("1.999.3")
         version.incrementMinor()
         Truth.assertThat(version.versionMajor).isEqualTo(2)
         Truth.assertThat(version.versionMinor).isEqualTo(0)
@@ -84,20 +129,23 @@ class VersionTest {
     }
 
     @Test(expected = RuntimeException::class)
-    fun invalidIncrementMinor() {
+    fun `GIVEN a version with the maximum possible major & minor WHEN creating invoking incrementMinor() THEN it throws an exception`() {
         val version = createVersion("999.999.3")
         version.incrementMinor()
     }
 
     @Test
-    fun incrementPatch() {
-        var version = createVersion("1.2.3")
+    fun `GIVEN a version WHEN creating invoking incrementPatch() THEN it increments the patch`() {
+        val version = createVersion("1.2.3")
         version.incrementPatch()
         Truth.assertThat(version.versionMajor).isEqualTo(1)
         Truth.assertThat(version.versionMinor).isEqualTo(2)
         Truth.assertThat(version.versionPatch).isEqualTo(4)
+    }
 
-        version = createVersion("1.2.999")
+    @Test
+    fun `GIVEN a version with the maximum patch WHEN creating invoking incrementPatch() THEN it increments minor and reset patch`() {
+        val version = createVersion("1.2.999")
         version.incrementPatch()
         Truth.assertThat(version.versionMajor).isEqualTo(1)
         Truth.assertThat(version.versionMinor).isEqualTo(3)
@@ -105,7 +153,7 @@ class VersionTest {
     }
 
     @Test(expected = RuntimeException::class)
-    fun invalidIncrementPatch() {
+    fun `GIVEN a version with the maximum possible major, minor & patch WHEN creating invoking incrementPatch() THEN it throws an exception`() {
         val version = createVersion("999.999.999")
         version.incrementPatch()
     }
