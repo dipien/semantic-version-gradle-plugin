a4810a5f92a98c071623e71aac08d2de1b00e49d
Maxi Rosson <maxirosson@gmail.com>
2020-06-28 09:49:59 -0300

    Set version on subprojects

--- semantic-version-gradle-plugin/src/main/java/com/semanticversion/gradle/plugin/SemanticVersionGradlePlugin.kt
+++ semantic-version-gradle-plugin/src/main/java/com/semanticversion/gradle/plugin/SemanticVersionGradlePlugin.kt
@@ -1,6 +1,7 @@
 package com.semanticversion.gradle.plugin
 
 import com.semanticversion.Version
+import com.semanticversion.common.GitHelper
 import com.semanticversion.common.PropertyResolver
 import com.semanticversion.gradle.plugin.commons.CommandExecutorImpl
 import com.semanticversion.gradle.plugin.commons.GitHelperImpl
@@ -18,10 +19,13 @@ open class SemanticVersionGradlePlugin : Plugin<Project> {
     protected lateinit var project: Project
     protected lateinit var extension: SemanticVersionGradlePluginExtension
     protected lateinit var propertyResolver: PropertyResolver
+    protected lateinit var gitHelper: GitHelper
+    protected lateinit var baseVersion: String
 
     override fun apply(project: Project) {
         this.project = project
         propertyResolver = PropertyResolverImpl(project)
+        gitHelper = GitHelperImpl(propertyResolver, CommandExecutorImpl(project, LogLevel.LIFECYCLE))
 
         if (project.version == Project.DEFAULT_VERSION) {
             project.version = project.rootProject.version
@@ -32,13 +36,15 @@ open class SemanticVersionGradlePlugin : Plugin<Project> {
             project.version = "0.1.0"
         }
 
-        val gitHelper = GitHelperImpl(propertyResolver, CommandExecutorImpl(project, LogLevel.LIFECYCLE))
-
-        val baseVersion = Version(project.version.toString()).baseVersion
+        baseVersion = Version(project.version.toString()).baseVersion
         val version = Version(propertyResolver, gitHelper, baseVersion)
         project.version = version.toString()
 
-        extension = project.extensions.create(EXTENSION_NAME, SemanticVersionGradlePluginExtension::class.java, propertyResolver, gitHelper, baseVersion)
+        project.subprojects.forEach {
+            it.version = version.toString()
+        }
+
+        extension = project.extensions.create(EXTENSION_NAME, SemanticVersionGradlePluginExtension::class.java)
 
         project.tasks.create(PrintVersionTask.TASK_NAME, PrintVersionTask::class.java)
         project.tasks.create(IncrementMajorVersionTask.TASK_NAME, IncrementMajorVersionTask::class.java)
--- semantic-version-gradle-plugin/src/main/java/com/semanticversion/gradle/plugin/SemanticVersionGradlePluginExtension.kt
+++ semantic-version-gradle-plugin/src/main/java/com/semanticversion/gradle/plugin/SemanticVersionGradlePluginExtension.kt
@@ -1,12 +1,3 @@
 package com.semanticversion.gradle.plugin
 
-import com.semanticversion.android.AndroidVersion
-import com.semanticversion.common.GitHelper
-import com.semanticversion.common.PropertyResolver
-
-open class SemanticVersionGradlePluginExtension(val propertyResolver: PropertyResolver, val gitHelper: GitHelper, val baseVersion: String) {
-
-    fun calculateVersionCode(): Int {
-        return AndroidVersion(propertyResolver, gitHelper, baseVersion).versionCode
-    }
-}
+open class SemanticVersionGradlePluginExtension
