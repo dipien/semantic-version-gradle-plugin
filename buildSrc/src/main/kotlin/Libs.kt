b52281ea0f994be26f993080963bd011d4f6dba0
dipien-ci <dipien2020@gmail.com>
2021-07-04 00:11:27 +0000

    Upgraded com.google.truth:truth from 1.0.1 to 1.1.3

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -6,5 +6,5 @@ object Libs {
     // https://github.com/junit-team/junit4/releases
     const val JUNIT = "junit:junit:4.13.2"
 
-    const val TRUTH = "com.google.truth:truth:1.0.1"
+    const val TRUTH = "com.google.truth:truth:1.1.3"
 }
