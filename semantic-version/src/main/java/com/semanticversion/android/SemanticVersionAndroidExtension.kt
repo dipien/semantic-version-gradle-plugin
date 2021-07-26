c2855edcf247592cf8dacabebdb52eed172e794c
Maxi Rosson <maxirosson@gmail.com>
2021-07-26 20:23:05 -0300

    Disable minSdkVersionAsVersionCodePrefix by default

--- semantic-version/src/main/java/com/semanticversion/android/SemanticVersionAndroidExtension.kt
+++ semantic-version/src/main/java/com/semanticversion/android/SemanticVersionAndroidExtension.kt
@@ -6,6 +6,6 @@ import com.semanticversion.common.PropertyResolver
 open class SemanticVersionAndroidExtension(propertyResolver: PropertyResolver) : SemanticVersionExtension(propertyResolver) {
 
     var versionCodePrefix: Int? = propertyResolver.getIntegerProp(::versionCodePrefix.name)
-    var minSdkVersionAsVersionCodePrefix: Boolean = propertyResolver.getRequiredBooleanProp(::minSdkVersionAsVersionCodePrefix.name, true)
+    var minSdkVersionAsVersionCodePrefix: Boolean = propertyResolver.getRequiredBooleanProp(::minSdkVersionAsVersionCodePrefix.name, false)
     var versionCodeExtraBit: Int = propertyResolver.getRequiredIntegerProp(::versionCodeExtraBit.name, 0)
 }
