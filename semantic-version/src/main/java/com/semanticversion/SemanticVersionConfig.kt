2e0f793df27b5d1c920e96b8adf94f1bf7c44542
Maxi Rosson <maxirosson@gmail.com>
2021-07-27 18:40:45 -0300

    Ktlint fix

--- semantic-version/src/main/java/com/semanticversion/SemanticVersionConfig.kt
+++ semantic-version/src/main/java/com/semanticversion/SemanticVersionConfig.kt
@@ -3,7 +3,8 @@ package com.semanticversion
 open class SemanticVersionConfig(
     var maximumVersion: Int? = null,
     var versionClassifier: String? = null,
-    var snapshot: Boolean? = null)
+    var snapshot: Boolean? = null
+)
 
 // var featureBranchPrefix: String? = propertyResolver.getStringProp(::featureBranchPrefix.name)
 // var versionTimestampEnabled: Boolean? = propertyResolver.getBooleanProp(::featureBranchPrefix.name)
