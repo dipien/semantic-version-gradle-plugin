690107e3c7257f7bb369ebf22e2d7764039734f7
Maxi Rosson <maxirosson@gmail.com>
2022-04-05 09:25:04 -0300

    Configuration improvements

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,14 @@ plugins {
     id("com.gradle.enterprise").version("3.9")
 }
 
+if (System.getenv("CI") == "true") {
+    buildCache {
+        local {
+            directory = File(System.getProperty("user.home"), "/gradle-build-cache")
+        }
+    }
+}
+
 include(":semantic-version")
 include(":semantic-version-gradle-plugin")
 include(":semantic-version-android-gradle-plugin")
-
-apply(from = java.io.File(settingsDir, "buildCacheSettings.gradle"))
