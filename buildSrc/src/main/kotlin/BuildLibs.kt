ba2cf199745cb60cd21385e93a5211452448fa8d
Maxi Rosson <maxirosson@gmail.com>
2022-07-20 09:27:26 -0300

    Fix versions

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -8,11 +8,11 @@ object BuildLibs {
     const val DIPIEN_COMPONENT_BUILDER = "com.dipien:dipien-component-builder:3.2.0"
 
     // https://github.com/dipien/release-gradle-plugin/blob/master/CHANGELOG.md
-    const val RELEASE_GRADLE_PLUGIN = "com.dipien:release-gradle-plugin:1.0.0"
+    const val RELEASE_GRADLE_PLUGIN = "com.dipien:release-gradle-plugin:1.1.0"
 
     // https://github.com/dipien/releases-hub-gradle-plugin/releases
     const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:3.1.0"
 
     // https://github.com/dipien/semantic-version-gradle-plugin/blob/master/CHANGELOG.md
-    const val SEMANTIC_VERSION_GRADLE_PLUGIN = "com.dipien:semantic-version-gradle-plugin:1.1.0"
+    const val SEMANTIC_VERSION_GRADLE_PLUGIN = "com.dipien:semantic-version-gradle-plugin:1.0.0"
 }
