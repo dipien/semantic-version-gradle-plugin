8c35951f616bd11e585f0285467f0c80830f4961
dipien-ci <dipien2020@gmail.com>
2023-02-24 18:38:26 +0000

    "Upgraded com.android.tools.build:gradle from 7.3.1 to 7.4.1"

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -6,7 +6,7 @@ object Libs {
     // https://github.com/maxirosson/jdroid-java/releases
     const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.1.0"
 
-    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:7.3.1"
+    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:7.4.1"
 
     // https://github.com/junit-team/junit4/releases
     const val JUNIT = "junit:junit:4.13.2"
